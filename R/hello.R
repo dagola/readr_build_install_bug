1670e03c9695020406149cef77ad4f48489b22bf
Dr. Damian Gola <d.gola@euroimmun.de>
2020-12-07 13:53:08 +0100

    Deactivate working code using read.csv

--- R/hello.R
+++ R/hello.R
@@ -1,8 +1,8 @@
 
 .onLoad <- function(libname, pkgname) {
-  data <- read.csv(
-    file = system.file("data.csv", package = "bar"),
-  )
+  # data <- read.csv(
+  #   file = system.file("data.csv", package = "bar"),
+  # )
   data <- readr::read_csv(
     file = system.file("data.csv", package = "bar"),
     col_types = readr::cols(.default = readr::col_character())
