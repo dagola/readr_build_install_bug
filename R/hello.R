835a7b624d92394c32825a4bb473d490981c640b
Dr. Damian Gola <d.gola@euroimmun.de>
2020-12-07 14:06:55 +0100

    Fix package name

--- R/hello.R
+++ R/hello.R
@@ -4,7 +4,7 @@
   #   file = system.file("data.csv", package = "bar"),
   # )
   data <- readr::read_csv(
-    file = system.file("data.csv", package = "bar"),
+    file = system.file("data.csv", package = "rbib"),
     col_types = readr::cols(.default = readr::col_character())
   )
   options("bar.data" = data)
